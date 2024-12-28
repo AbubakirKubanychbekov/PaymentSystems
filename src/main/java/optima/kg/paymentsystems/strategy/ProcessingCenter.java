e1fa9431a321516289063f17c33e67f8fc390137
Abubakir <kubanycbekovabubakir@gmail.com>
2024-12-28 12:04:56 +0600

    processing center

---
+++ src/main/java/optima/kg/paymentsystems/strategy/ProcessingCenter.java
@@ -0,0 +1,14 @@
+package optima.kg.paymentsystems.strategy;
+
+import optima.kg.paymentsystems.dto.entity.Card;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Abubakir Dev
+ */
+public interface ProcessingCenter {
+    void issueCard(Card card);
+    void replenishment(Card card, BigDecimal amount);
+    void withdraw(Card card, BigDecimal amount);
+}
